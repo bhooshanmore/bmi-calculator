9563703126080cb419a1475c3f724874a566f40d
BhushanM <bhooshanmore@gmail.com>
2021-10-26 18:09:40 +0100

    added Grunt for building application

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function (grunt) {
+
+    // initConfig  configuration  
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                src: ['app.js', 'utility/utility.js', 'routes/app-router.js'],
+                dest: 'dist/built.js',
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'concat/<%= pkg.name %>.js'
+            }
+        },
+        qunit: {
+            files: ['test/*.html']
+        },
+
+
+        clean: {
+            src: ['concat','uglify']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // "grunt default task
+    grunt.registerTask('default', ['uglify', 'concat']);
+};
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = function (grunt) {
+//     // Project configuration.
+//     grunt.initConfig({
+//         concat: {
+//             options: {
+//                 separator: ';',
+//             },
+//             dist: {
+//                 src: ['app.js', 'utility/utility.js', 'routes/app-router.js'],
+//                 dest: 'dist/built.js',
+//             },
+//         },
+//         watch: {
+//             js: {
+//               files: [
+//                 '<%= jshint.all %>'
+//               ],
+//               tasks: ['concat','jshint','uglify']
+//             }
+//           },
+//           clean: {
+//             dist: [
+//               'assets/js/scripts.min.js'
+//             ]
+//           }
+//     });
+// }
+
+// grunt.registerTask('dev', [
+//     'watch'
+// ]);
